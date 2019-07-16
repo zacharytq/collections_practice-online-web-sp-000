7346bfcf2e8da618b2ffa1b3cf2c86cbf25c7d57
chic-page-6502 <zacharytq@gmail.com>
2019-07-16 00:26:44 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,15 @@
+def sort_array_asc(array)
+  array.sort do |a, b|
+    a <=> b
+  end
+end
 
+def sort_array_desc(array)
+  sort_array_asc(array).reverse
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
