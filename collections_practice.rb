f7e4af9fb00c170a743d1f326750afbc21210b6f
chic-page-6502 <zacharytq@gmail.com>
2019-07-16 00:47:38 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -15,8 +15,5 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-  holder_array = array
-  array[1] = holder_array[2]
-  array[2] = holder_array[1]
-  array
+  array[1], array[2] = array[2], array[1]
 end
